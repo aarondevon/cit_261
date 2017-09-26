8ca7346e73f46ba7c44d3836d1f5d96c77406158
aarondevon <me@aarondevon.com>
2017-09-25 20:25:55 -0500

    add comments

--- fahrenheit_to_celsius.js
+++ fahrenheit_to_celsius.js
@@ -1,8 +1,10 @@
-// constant variable decleration
+// constant variable initialization get temp from user
 const celsius = prompt('What is the celsius temperature today?')
 
+// convert celsius to fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
 
+// round fahrenheit temp
 fahrenheit = Math.floor(fahrenheit);
 
 alert('The temperature is ' + fahrenheit + ' degrees fahrenheit.')
