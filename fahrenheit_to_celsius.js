4ec325a5385631cc45a60abe7b3a630409799fa2
aarondevon <me@aarondevon.com>
2017-09-25 20:23:29 -0500

    celsius to fahrenheit

---
+++ fahrenheit_to_celsius.js
@@ -0,0 +1,8 @@
+// constant variable decleration
+const celsius = prompt('What is the celsius temperature today?')
+
+let fahrenheit = celsius * (9/5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+
+alert('The temperature is ' + fahrenheit + ' degrees fahrenheit.')
